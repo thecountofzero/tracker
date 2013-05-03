3cf8979a5df2537cf7d47093be40c3e265af06d7
Mike Malamud <mike.malamud@gmail.com>
2013-05-03 14:21:50 -0700

    Adding new files for EJS Helper hooks

---
+++ tracker-ejs-helpers.js
@@ -0,0 +1,40 @@
+steal('can/view/ejs', 'tracker', function(ejs, Tracker) {
+
+    can.EJS.Helpers.prototype.tracked_input = function(obs, attr, props) {
+        return "<input " + can.view.hook(function(el) {
+            el.value = obs.attr(attr);
+            el.id = attr;
+            el.name = attr;
+            can.$(el).data('tracker', new Tracker(can.$(el), {
+                linkedObj: obs,
+                changeCallback: props && props.changeCallback ? props.changeCallback : can.noop
+            }));
+        }) + "/>";
+    };
+
+    can.EJS.Helpers.prototype.tracked_select = function(options, obs, attr, props) {
+        
+
+
+        var htmlStr = "<select " + can.view.hook(function(el) {
+            el.id = attr;
+            el.name = attr;
+            if(can.$(el).data('tracker')) {
+                can.$(el).data('tracker').destroy();
+            }
+
+            can.$(el).data('tracker', new Tracker(can.$(el), {
+                linkedObj: obs,
+                changeCallback: props && props.changeCallback ? props.changeCallback : can.noop
+            }));
+        }) + ">";
+        for(var i=0; i<options.length; ++i) {
+            var opt = options[i];
+            htmlStr += "<option value='" + opt.value + "' " + (opt.value===obs.attr(attr)?'selected':'') + ">" + opt.text + "</option>";
+        }
+
+        return htmlStr + "</select>";
+    };
+
+    return can.EJS.Helpers.prototype;
+});
